07f8103f2b59b7bfc843c57620bf08bcd61f06a6
musaddiq-ciklum <82140081+musaddiq-ciklum@users.noreply.github.com>
2021-06-25 11:10:26 +0500

    Fix spell issue

--- database/migrations/2021_06_09_210731_create_product_variants_table.php
+++ database/migrations/2021_06_09_210731_create_product_variants_table.php
@@ -24,7 +24,7 @@ class CreateProductVariantsTable extends Migration
             $table->decimal('msrp', 8, 2)->default(null);
             $table->integer('stock');
             $table->boolean('in_stock')->default('1');
-            $table->boolean('isS_default')->default('1');
+            $table->boolean('is_default')->default('1');
             $table->boolean('active')->default('1');
             $table->timestamps();
         });
