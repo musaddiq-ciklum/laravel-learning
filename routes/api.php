be5bcad6ace92bf668f1a52d2a3a4c3d38f3b1c1
musaddiq-ciklum <muur@ciklum.com>
2021-08-04 18:26:24 +0500

    Api Authentication Using Sanctum

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+
+Route::apiResource('products', ProductController::class);
+
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    Route::apiResource('products', ProductController::class);
+
+    Route::post('upload-file',[UserController::class,'uploadFile']);
+});;
+
+Route::post("login",[UserController::class,'login']);
